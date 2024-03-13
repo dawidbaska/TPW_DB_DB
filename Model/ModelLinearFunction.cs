585bfd190aec5212926b68226abf73cbb9da6bb5
DawidBanburski <161617177+DawidBanburski@users.noreply.github.com>
2024-03-13 15:50:39 +0100

    dodanie linearfunction and changes in xaml

---
+++ Model/ModelLinearFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPW_DB_DB.Model
+{
+    public class ModelLinearFunction
+    {
+        public double a;
+        public double b;
+    
+
+    public double FindZero(double a, double b)
+    {
+        return -b / a;
+    }
+
+    }
+}
