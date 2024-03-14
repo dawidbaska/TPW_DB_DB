c63134b73362e106ce2379a9f0840036347af199
dawidbaska <161619305+dawidbaska@users.noreply.github.com>
2024-03-14 16:01:09 +0100

    Testy

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using TPW_DB_DB.Model;
+
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ModelLinearFunction function = new ModelLinearFunction();
+            function.a = 1;
+            function.b = 2;
+            Assert.AreEqual(function.FindZero(), -2);
+            function.a = 0;
+            Assert.AreEqual(function.FindZero(), null);
+
+
+
+        }
+    }
+}
