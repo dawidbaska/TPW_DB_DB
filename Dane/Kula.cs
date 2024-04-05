71c1459ba885d2d1dd32d8683fb36ed4d50a36b2
dawidbaska <161619305+dawidbaska@users.noreply.github.com>
2024-04-05 13:39:47 +0200

    Etap 1 start

---
+++ Dane/Kula.cs
@@ -0,0 +1,15 @@
+﻿namespace Dane
+{
+    public class Kula
+    {
+        private int x;
+        private int y;
+
+        public Kula()
+        {
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+    }
+}
---
+++ Logika/Kula.cs
@@ -0,0 +1,13 @@
+﻿namespace Logika
+{
+    public class Kula
+    {
+        public void losujstart()
+        {
+            var rand = new Random();
+            int x1 = rand.Next(-1, 1);
+            int y1 = rand.Next(-1, 1);
+            
+        }
+    }
+}
