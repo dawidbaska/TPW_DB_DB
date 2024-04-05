cc9f726febd5150a509a5161b21e01c576237989
Dawid Banburski <247620@edu.p.lodz.pl>
2024-04-05 14:12:34 +0200

    nowy start

---
+++ TPW_DB_DB/Dane/Kula.cs
@@ -0,0 +1,12 @@
+﻿namespace Dane
+{
+    public class Kula
+    {
+        private int x;
+        private int y;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+    }
+
+}
