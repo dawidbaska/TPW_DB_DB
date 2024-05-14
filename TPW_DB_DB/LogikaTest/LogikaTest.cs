e7fcab62c39b024cb73aff9514e2bb67117dbb5d
Dawid Banburski <247620@edu.p.lodz.pl>
2024-05-14 18:34:15 +0200

    logika testy

--- TPW_DB_DB/LogikaTest/LogikaTest.cs
+++ TPW_DB_DB/LogikaTest/LogikaTest.cs
@@ -1,51 +1,65 @@
+using Dane;
+using Logika;
+using System.Reflection;
+
 namespace LogikaTest
 {
     [TestClass]
     public class LogikaTest
     {
-        /*  
+
         [TestMethod]
         public void TestDodajKula()
         {
             Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
-            kule.DodajKula();
-            Assert.AreEqual(1,kule.ListaGetSize());
+            kule.DodajKula(10,10,10);
+            Assert.AreEqual(1, kule.ListaGetSize());
         }
 
         [TestMethod]
         public void TestLosujStar()
         {
             Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
-            kule.DodajKula();
+            kule.DodajKula(10,10,10);
             kule.LosujStart(0, 100, 0, 100, 0);
             Dane.Kula test = kule.GetKula(0);
             Assert.IsTrue(test.X >= 0 && test.X <= 100);
             Assert.IsTrue(test.Y >= 0 && test.Y <= 100);
-        }
-        [TestMethod]
-        public void TestLosujNowaPozycja()
-        {
-            Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
-            kule.DodajKula();
-            kule.LosujStart(0, 100, 0, 100, 0);
-            Dane.Kula test = kule.GetKula(0);
-            kule.LosujNowaPozycja(0, 100, 0, 100, 0);
-            Dane.Kula test1 = kule.GetKula(0);
-            int dX = test1.X - test.X;
-            int dY = test1.Y - test.Y;
-            Assert.IsTrue(dX >= -1 && dX <= 1);
-            Assert.IsTrue(dY >= -1 && dY <= 1);
         }
 
         [TestMethod]
         public void TestListaClear()
         {
             Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
-            kule.DodajKula();
+            kule.DodajKula(10,10,10);
             Assert.AreEqual(1, kule.ListaGetSize());
             kule.ListaClear();
             Assert.AreEqual(0, kule.ListaGetSize());
         }
-        */
+
+        [TestMethod]
+        public void TestStworzPlansze()
+        {
+            Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
+            Dane.Plansza plansza = kule.StworzPlansze(1, 2, 3);
+            Assert.AreEqual(1, plansza.W);
+            Assert.AreEqual(2, plansza.H);
+            Assert.AreEqual(3, plansza.BT);
+        }
+
+
+        [TestMethod]
+        public void TestLosujNowaPozycja()
+        {
+            Logika.LogikaAPI kule = new Logika.Logika(Dane.DaneAPI.Stworz());
+            kule.DodajKula(10,10,10);
+            Dane.Kula kula = kule.GetKula(0);
+            kula.X = -1;
+            kula.Y = -2;
+            kule.LosujStart(0, 100, 0, 100, 0);
+            Dane.Kula kula1 = kule.GetKula(0);
+            Assert.IsTrue(kula.X >= 0 && kula.X <= 100);
+            Assert.IsTrue(kula.Y >= 0 && kula.Y <= 100);
+        }
     }
 }
